6f9906ba90fcdc2de5c9fb071a7ffb1f3ec8f5e6
Kestutis <kestutis.stalmokas@gmail.com>
2015-02-27 10:07:23 -0800

    Fix wiredep's cwd

--- Gruntfile.js
+++ Gruntfile.js
@@ -157,7 +157,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       options: {
-        cwd: '<%= yeoman.app %>'
+        cwd: './'
       },
       app: {
         src: ['<%= yeoman.app %>/index.html'],
